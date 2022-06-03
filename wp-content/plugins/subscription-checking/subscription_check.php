971974ff12dd658a3bfa72947cc3a8dfa785283b
Rhittick <rhittickrc@codtrees.com>
2022-06-03 19:44:40 +0530

    subscripton check

---
+++ wp-content/plugins/subscription-checking/subscription_check.php
@@ -0,0 +1,18 @@
+<?php 
+/** 
+ * @package Subscriptionchecking
+ */
+/*
+Plugin Name: Subscription-checking
+Plugin URI: http://Subscription-checking.com
+Description: This is the custom plugin for subscription checking user in  PMP
+Version: 1.0.0
+Author: Kode Tiger
+Author URI: http://www.kodetiger.io
+License: GPLv2 or later
+Text Domain: Subscription-checking
+*/ 
+// Exit if accessed directly
+
+
+echo do_shortcode('[subscription_check]');
